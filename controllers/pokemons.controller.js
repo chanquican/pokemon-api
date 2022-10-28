ff80984d76ac7f042242901294c76ef8cf1b68ad
jose <jose.carmonas@utem.cl>
2022-10-28 17:21:39 -0300

    agregar atributo peso-paginación-capturar id

--- controllers/pokemons.controller.js
+++ controllers/pokemons.controller.js
@@ -7,6 +7,7 @@ const getPokemon = async(id) => {
         id: resp.id,
         name: resp.forms[0].name,
         height: resp.height,
+        weight: resp.weight,
         types: [],
         abilities: [],
         sprites: resp.sprites.front_default,
@@ -35,11 +36,31 @@ const getPokemon = async(id) => {
 
 
 let getAll = async(request, response) => {
-    let datos = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=%5bstart%5d&limit=%5bend`);
+    //let args = request.body;
+    console.log(request.query.page);
+    let page = request.query.page - 1;
+    let limit = 10;
+    let offset = limit * page; //100%10 -> 990 / 99
+    //console.log(offset);
+    //console.log(start);
+    //let datos = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=20&start=21&limit=10`);
+    let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    //console.log(url);
+    let datos = await axios.get(url);
+    // [ {name:pokemon, url:url }, {}, {}]
     //console.log(datos.data);
     let pokemons = [];
-    for (i = 1; i <= 151; i++) {
-        pokemons.push(await getPokemon(i));
+    //recorremos solo pokemons encontrados en url = 10
+    for (i = 0; i < datos.data.results.length; i++) {
+        try {
+            //recuperar id pokemon desde string url
+            let pokemonId = datos.data.results[i].url.split('/');
+            //console.log(pokemonId[6]);
+            pokemons.push(await getPokemon(pokemonId[6]));
+        } catch (error) {
+            console.log(error.message);
+            console.log(`pokemon id ${i} not found`);
+        }
     }
 
     // handle success
