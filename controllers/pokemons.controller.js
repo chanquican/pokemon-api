dc2c6ea5e120b5d0ed162fd60c92f439f1f378ef
jose <jose.carmonas@utem.cl>
2022-10-28 01:58:04 -0300

    get 151 pokemons

--- controllers/pokemons.controller.js
+++ controllers/pokemons.controller.js
@@ -1,7 +1,52 @@
+const axios = require('axios');
 "use strict";
+const getPokemon = async(id) => {
+    let resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    resp = resp.data;
+    let pokemon = {
+        id: resp.id,
+        name: resp.forms[0].name,
+        height: resp.height,
+        types: [],
+        abilities: [],
+        sprites: resp.sprites.front_default,
+
+    }
+
+    resp.abilities.forEach(habilidad => {
+
+        pokemon.abilities.push({
+            name: habilidad.ability.name,
+            hidden: habilidad.is_hidden
+        });
+    });
+
+    resp.types.forEach(tipo => {
+
+        pokemon.types.push({
+            type: tipo.type.name
+        });
+    });
+
+    //console.log(pokemon);
+    return (pokemon);
+
+}
+
 
 let getAll = async(request, response) => {
-    response.status(200).send("All pokemons");
+    let datos = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=%5bstart%5d&limit=%5bend`);
+    //console.log(datos.data);
+    let pokemons = [];
+    for (i = 1; i <= 151; i++) {
+        pokemons.push(await getPokemon(i));
+    }
+
+    // handle success
+
+    //console.log(response);
+
+    response.send(pokemons);
 };
 
 module.exports = {
